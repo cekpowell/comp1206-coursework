b8e63466bfe196edc4d068f706dd92040b299ac8
cp6g18 <cp6g18@soton.ac.uk>
2021-08-18 21:52:47 +0100

    Allow for packaging into .jar

---
+++ src/main/java/Entry.java
@@ -0,0 +1,14 @@
+/**
+ * Entry point for program (work around for compiling JavaFX into Jar).
+ */
+public class Entry {
+
+    /**
+     * Main method - runs the main program to start the application.
+     * @param  args System arguments.
+     */
+    public static void main(String[] args) {
+        Mathdoku.main(args);
+    }
+}
+
